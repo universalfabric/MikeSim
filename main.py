a8361a3328d4944100e577eb6500ad6f39af67de
universalfabric <mike.freimuth@gmail.com>
2019-03-09 21:50:53 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+This is my main file for a mess around project.
+
+Should be very simple.
+
+Objective is to use code knowledge to date outside of the regular class structure.
+
+Ideas:
+
+1. Simple combat, 1 v 1
+    Create player object and monster object.
+    determine a turn system.
+    give player and monster attributes, armor class, weapons, etc.
+    implement in a progressive way, adding complexity along the way.
+
+"""
